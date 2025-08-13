cd5ce963e9cebf143abf2cb5f90c115e3eb0c1a3
Ritesh23-CS <riteshkashid94@gmail.com>
2025-08-13 19:38:00 +0530

    Initial commit

---
+++ react-mini-project/src/InfoBox.jsx
@@ -0,0 +1,42 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css"
+export default function InfoBox({info}){
+    const INIT_URL = "https://images.unsplash.com/photo-1584267385494-9fdd9a71ad75?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    const RAIN_URL = "https://images.unsplash.com/photo-1492011221367-f47e3ccd77a0?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    const HOT_URL = "https://images.unsplash.com/uploads/14121010130570e22bcdf/e1730efe?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const COLD_URL = "https://images.unsplash.com/photo-1674407866481-a39b2239f771?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+
+
+    return(
+        <div className='InfoBox'>
+            <h1>Weather Information-{info.weather}</h1>
+        <div className='CardContainer'>
+    <Card sx={{ maxWidth: 345 }}>
+    <CardMedia
+        sx={{ height: 140 }}
+        image={info.humidity > 80 ? RAIN_URL : info.temp>15 ? HOT_URL:COLD_URL}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.city}
+        </Typography>
+        <Typography variant="body2" color='text.secondary' component={"span"} >
+            <p>Temperature = {info.temp}&deg;C</p>
+            <p>Humidity = {info.humidity}</p>
+            <p>Min Temp = {info.tempMin}&deg;C</p>
+            <p>Max Temp = {info.tempMax}&deg;C</p>
+            <p>The weather can be described as <i>{info.weather} and feels like </i>{info.feelsLike}&deg;C</p>
+
+        
+        </Typography>
+      </CardContent>
+
+    </Card>  
+    </div>
+    </div>
+    )
+}
